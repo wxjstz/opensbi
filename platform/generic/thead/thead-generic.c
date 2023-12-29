0549842da0289888fcce3e29f0ade514de46b2d9
Xiang W <wxjstz@126.com>
2023-12-29 21:07:54 +0800

    platform: generic: thead: add ecall_light
    refer from: https://github.com/revyos/thead-opensbi/pull/5/commits/f880f3e8ff1da92392cfad61eb1e8a7496f75a54
    
    Signed-off-by: Xiang W <wxjstz@126.com>

--- platform/generic/thead/thead-generic.c
+++ platform/generic/thead/thead-generic.c
@@ -12,6 +12,7 @@
 #include <thead/c9xx_pmu.h>
 #include <sbi/sbi_const.h>
 #include <sbi/sbi_console.h>
+#include <sbi/sbi_ecall.h>
 #include <sbi/sbi_platform.h>
 #include <sbi/sbi_scratch.h>
 #include <sbi/sbi_string.h>
@@ -19,7 +20,7 @@
 #include <sbi_utils/fdt/fdt_helper.h>
 
 extern const struct sbi_hsm_device light_ppu;
-
+extern struct sbi_ecall_extension ecall_light;
 struct thead_generic_quirks {
 	u64	errata;
 };
@@ -196,11 +197,15 @@ static int thead_vendor_ext_provider(long funcid,
 	return 0;
 }
 
+bool request_ecall_light = false;
 
 static int thead_generic_final_init(bool cold_boot)
 {
-	if (cold_boot)
+	if (cold_boot) {
 		sbi_hsm_set_device(&light_ppu);
+		if (request_ecall_light)
+			sbi_ecall_register_extension(&ecall_light);
+	}
 
 	return 0;
 }
@@ -219,6 +224,8 @@ static int thead_generic_platform_init(const void *fdt, int nodeoff,
 		generic_platform_ops.extensions_init = thead_pmu_extensions_init;
 	if (quirks->errata & THEAD_QUIRK_ERRATA_LOGHT_PPU)
 		generic_platform_ops.final_init = thead_generic_final_init;
+	if (!sbi_strcmp(match->compatible, "thead,light"))
+		request_ecall_light = true;
 
 	return 0;
 }
