dde75e57f0938d3339b689def655780774d9e26e
Han Gao <gaohan@iscas.ac.cn>
2024-09-20 16:53:46 +0800

    sync sdk v2.0.2 code
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- lib/sbi/sbi_trap.c
+++ lib/sbi/sbi_trap.c
@@ -318,8 +318,16 @@ struct sbi_trap_context *sbi_trap_handler(struct sbi_trap_context *tcntx)
 		if (sbi_hart_has_extension(sbi_scratch_thishart_ptr(),
 					   SBI_HART_EXT_SMAIA))
 			rc = sbi_trap_aia_irq();
-		else
+		else {
+			/*
+			* DCACHE.CALL:
+			* | 31 - 25 | 24 - 20 | 19 - 15 | 14 - 12 | 11 - 7 | 6 - 0 |
+			*   0000000    00001     00000      000      00000  0001011
+			*/
+			if ((regs->mepc & 0x7f) == 4)
+				asm volatile(".long 0x0010000b\n");
 			rc = sbi_trap_nonaia_irq(mcause & ~MCAUSE_IRQ_MASK);
+		}
 		msg = "unhandled local interrupt";
 		goto trap_done;
 	}
--- platform/generic/thead/thead-generic.c
+++ platform/generic/thead/thead-generic.c
@@ -269,6 +269,9 @@ static void sbi_thead_reserved_pmp_set(void)
 	unsigned int num, reg_val;
 
 	for (num = 0; num < 4; num++) {
+		if (__sbi_hsm_hart_get_state(num) == SBI_HSM_STATE_STOPPED)
+			continue;
+
 		/*	pmp entry 28 for reserved memory	*/
 		writel(RESERVED_START_ADDR >> 12, (void *)(PMP_ENTRY_START_ADDR(28) + num*PMP_SIZE_PER_CORE));
 		writel(RESERVED_END_ADDR >> 12, (void *)(PMP_ENTRY_END_ADDR(28) + num*PMP_SIZE_PER_CORE));
@@ -291,12 +294,18 @@ static void sbi_thead_tcm0_pmp_set(unsigned long auth)
 
 	if (reg_val != TCM0_START_ADDR >> 12)
 		for(num = 0; num < 4; num++) {
+			if (__sbi_hsm_hart_get_state(num) == SBI_HSM_STATE_STOPPED)
+				continue;
+
 			/*	pmp entry 26 for dsp tcm0	*/
 			writel(TCM0_START_ADDR >> 12, (void *)(PMP_ENTRY_START_ADDR(26) + num*PMP_SIZE_PER_CORE));
 			writel(TCM0_END_ADDR >> 12, (void *)(PMP_ENTRY_END_ADDR(26) + num*PMP_SIZE_PER_CORE));
 		}
 
 	for(num = 0; num < 4; num++) {
+		if (__sbi_hsm_hart_get_state(num) == SBI_HSM_STATE_STOPPED)
+			continue;
+
 		/*	pmp entry 26 config	*/
 		reg_val = readl((void *)(PMP_ENTRY_CFG_ADDR(26) + num*PMP_SIZE_PER_CORE));
 		reg_val = (reg_val & 0xff00ffff) | (auth << 16);
@@ -314,12 +323,18 @@ static void sbi_thead_tcm1_pmp_set(unsigned long auth)
 	reg_val = readl((void *)PMP_ENTRY_START_ADDR(27));
 	if (reg_val != TCM1_START_ADDR >> 12)
 		for (num = 0; num < 4; num++) {
+			if (__sbi_hsm_hart_get_state(num) == SBI_HSM_STATE_STOPPED)
+				continue;
+
 			/*	pmp entry 27 for dsp tcm1	*/
 			writel(TCM1_START_ADDR >> 12, (void *)(PMP_ENTRY_START_ADDR(27) + num*PMP_SIZE_PER_CORE));
 			writel(TCM1_END_ADDR >> 12, (void *)(PMP_ENTRY_END_ADDR(27) + num*PMP_SIZE_PER_CORE));
 		}
 
 	for (num = 0; num < 4; num++) {
+		if (__sbi_hsm_hart_get_state(num) == SBI_HSM_STATE_STOPPED)
+			continue;
+
 		/*	pmp entry 27 config	*/
 		reg_val = readl((void *)(PMP_ENTRY_CFG_ADDR(27) + num*PMP_SIZE_PER_CORE));
 		reg_val = (reg_val & 0x00ffffff) | (auth << 24);
@@ -431,19 +446,21 @@ static int thead_generic_platform_init(const void *fdt, int nodeoff,
 	return 0;
 }
 
-/*
+
 static struct thead_generic_quirks thead_th1520_quirks = {
-	.errata = THEAD_QUIRK_ERRATA_TLB_FLUSH | THEAD_QUIRK_ERRATA_THEAD_PMU,
+	.errata = THEAD_QUIRK_ERRATA_LOGHT_PPU,
 };
-*/
+
 
 static const struct thead_generic_quirks thead_pmu_quirks = {
 	.errata = THEAD_QUIRK_ERRATA_THEAD_PMU,
 };
 
+/*
 static struct thead_generic_quirks thead_light_quirks = {
 	.errata = THEAD_QUIRK_ERRATA_TLB_FLUSH | THEAD_QUIRK_ERRATA_LOGHT_PPU,
 };
+*/
 
 static const struct fdt_match thead_generic_match[] = {
 	{ .compatible = "canaan,kendryte-k230", .data = &thead_pmu_quirks },
@@ -452,8 +469,8 @@ static const struct fdt_match thead_generic_match[] = {
 	{ .compatible = "sophgo,sg2000", .data = &thead_pmu_quirks },
 	{ .compatible = "sophgo,sg2002", .data = &thead_pmu_quirks },
 	{ .compatible = "sophgo,sg2044", .data = &thead_pmu_quirks },
-	{ .compatible = "thead,th1520", .data = &thead_light_quirks },
-        { .compatible = "thead,light", .data = &thead_light_quirks },
+	{ .compatible = "thead,th1520", .data = &thead_th1520_quirks },
+        { .compatible = "thead,light", .data = &thead_th1520_quirks },
 	{ },
 };
 
