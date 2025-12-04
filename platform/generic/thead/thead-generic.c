e96d9e2c465c8c58722d4912f0c4a0a26e470159
Xiang W <wxjstz@126.com>
2025-12-04 18:42:06 +0800

    platform: thead: Rework the platform operations of thead
    Rework the platform operations of thead to enhance flexibility.
    
    Signed-off-by: Xiang W <wangxiang@iscas.ac.cn>

--- platform/generic/thead/thead-generic.c
+++ platform/generic/thead/thead-generic.c
@@ -19,14 +19,17 @@ struct thead_generic_quirks {
 	u64	errata;
 };
 
-static int thead_tlb_flush_early_init(bool cold_boot)
+static u64 errata;
+
+static int thead_early_init(bool cold_boot)
 {
-	thead_register_tlb_flush_trap_handler();
+	if (errata & THEAD_QUIRK_ERRATA_TLB_FLUSH)
+		thead_register_tlb_flush_trap_handler();
 
 	return generic_early_init(cold_boot);
 }
 
-static int thead_pmu_extensions_init(struct sbi_hart_features *hfeatures)
+static int thead_extensions_init(struct sbi_hart_features *hfeatures)
 {
 	int rc;
 
@@ -34,7 +37,8 @@ static int thead_pmu_extensions_init(struct sbi_hart_features *hfeatures)
 	if (rc)
 		return rc;
 
-	thead_c9xx_register_pmu_device();
+	if (errata & THEAD_QUIRK_ERRATA_THEAD_PMU)
+		thead_c9xx_register_pmu_device();
 
 	return 0;
 }
@@ -43,11 +47,10 @@ static int thead_generic_platform_init(const void *fdt, int nodeoff,
 				       const struct fdt_match *match)
 {
 	const struct thead_generic_quirks *quirks = match->data;
+	errata = quirks->errata;
 
-	if (quirks->errata & THEAD_QUIRK_ERRATA_TLB_FLUSH)
-		generic_platform_ops.early_init = thead_tlb_flush_early_init;
-	if (quirks->errata & THEAD_QUIRK_ERRATA_THEAD_PMU)
-		generic_platform_ops.extensions_init = thead_pmu_extensions_init;
+	generic_platform_ops.early_init = thead_early_init;
+	generic_platform_ops.extensions_init = thead_extensions_init;
 
 	return 0;
 }
